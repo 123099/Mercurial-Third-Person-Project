ea0258dbf1819df9287eb0902528e6e05c0a2bbe
Orgoth01 <cristian.ionescu96@gmail.com>
2017-02-16 13:27:17 +0100

    the stuff you don;t like

--- MGE/src/Engine/Core/Behaviours/PlayerInput.cpp
+++ MGE/src/Engine/Core/Behaviours/PlayerInput.cpp
@@ -8,9 +8,113 @@ void PlayerInput::Awake()
 }
 
 void PlayerInput::Update()
-{
+{/*
 	if (Input::IsKeyPressed(sf::Keyboard::A))
 	{
 		m_textLog->setLogInfo("A");
 	}
+	if (Input::IsKeyPressed(sf::Keyboard::B))
+	{
+		m_textLog->setLogInfo("B");
+	}
+	if (Input::IsKeyPressed(sf::Keyboard::C))
+	{
+		m_textLog->setLogInfo("C");
+	}
+	if (Input::IsKeyPressed(sf::Keyboard::D))
+	{
+		m_textLog->setLogInfo("D");
+	}
+	if (Input::IsKeyPressed(sf::Keyboard::E))
+	{
+		m_textLog->setLogInfo("E");
+	}
+	if (Input::IsKeyPressed(sf::Keyboard::F))
+	{
+		m_textLog->setLogInfo("F");
+	}
+	if (Input::IsKeyPressed(sf::Keyboard::G))
+	{
+		m_textLog->setLogInfo("G");
+	}
+	if (Input::IsKeyPressed(sf::Keyboard::H))
+	{
+		m_textLog->setLogInfo("H");
+	}
+	if (Input::IsKeyPressed(sf::Keyboard::I))
+	{
+		m_textLog->setLogInfo("I");
+	}
+	if (Input::IsKeyPressed(sf::Keyboard::J))
+	{
+		m_textLog->setLogInfo("J");
+	}
+	if (Input::IsKeyPressed(sf::Keyboard::K))
+	{
+		m_textLog->setLogInfo("K");
+	}
+	if (Input::IsKeyPressed(sf::Keyboard::L))
+	{
+		m_textLog->setLogInfo("L");
+	}
+	if (Input::IsKeyPressed(sf::Keyboard::M))
+	{
+		m_textLog->setLogInfo("M");
+	}
+	if (Input::IsKeyPressed(sf::Keyboard::N))
+	{
+		m_textLog->setLogInfo("N");
+	}
+	if (Input::IsKeyPressed(sf::Keyboard::O))
+	{
+		m_textLog->setLogInfo("O");
+	}
+	if (Input::IsKeyPressed(sf::Keyboard::P))
+	{
+		m_textLog->setLogInfo("P");
+	}
+	if (Input::IsKeyPressed(sf::Keyboard::Q))
+	{
+		m_textLog->setLogInfo("Q");
+	}
+	if (Input::IsKeyPressed(sf::Keyboard::R))
+	{
+		m_textLog->setLogInfo("R");
+	}
+	if (Input::IsKeyPressed(sf::Keyboard::S))
+	{
+		m_textLog->setLogInfo("S");
+	}
+	if (Input::IsKeyPressed(sf::Keyboard::T))
+	{
+		m_textLog->setLogInfo("T");
+	}
+	if (Input::IsKeyPressed(sf::Keyboard::U))
+	{
+		m_textLog->setLogInfo("U");
+	}
+	if (Input::IsKeyPressed(sf::Keyboard::V))
+	{
+		m_textLog->setLogInfo("V");
+	}
+	if (Input::IsKeyPressed(sf::Keyboard::W))
+	{
+		m_textLog->setLogInfo("W");
+	}
+	if (Input::IsKeyPressed(sf::Keyboard::X))
+	{
+		m_textLog->setLogInfo("X");
+	}
+	if (Input::IsKeyPressed(sf::Keyboard::Y))
+	{
+		m_textLog->setLogInfo("Y");
+	}
+	if (Input::IsKeyPressed(sf::Keyboard::Z))
+	{
+		m_textLog->setLogInfo("Z");
+	}
+	if (Input::IsKeyPressed(sf::Keyboard::Space))
+	{
+		m_textLog->setLogInfo(" ");
+	}*/
 }
